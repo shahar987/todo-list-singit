b88e99e69e460ddc03e2727fe784d6d18de36798
Shahar <theLittleCitizen12@gmail.com>
2023-08-07 15:06:39 +0300

    created edit feature

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var taskData = [];
+let taskData = [];
 
 function uniqueId() {
   const dateString = Date.now().toString(36);
@@ -6,12 +6,44 @@ function uniqueId() {
   return dateString + randomness;
 }
 
-function deleteTask(taskId) {
-  const filtered = taskData.filter((task) => {
-    task.id !== taskId;
+function saveEdit(taskId){
+    const task = taskData.find((item) => item.id === taskId);
+    const val = document.querySelector("#edit-task").value;
+    if (task) {
+        task.content = val;
+        clearData();
+        showTasks();
+    }
+}
+
+function editTask(taskId) {
+  taskData.map((task) => {
+    if (task.id === taskId) {
+      if (!task.isEditing) {
+
+        const itemList = document.querySelector(`#${taskId}`);
+        while (itemList.firstChild) {
+          itemList.removeChild(itemList.firstChild);
+        }
+        var input = document.createElement("input");
+        input.value = task.content
+        input.id="edit-task"
+        var checkIcon = document.createElement("i");
+        checkIcon.className="fa fa-check"
+        checkIcon.addEventListener("click", function handleClick(event) {
+            const taskId = event.target.parentElement.id;
+            saveEdit(taskId);
+          });
+        itemList.appendChild(input);
+        itemList.appendChild(checkIcon);
+      }
+    }
   });
+}
+
+function deleteTask(taskId) {
+  const filtered = taskData.filter((task) => task.id !== taskId);
   taskData = filtered;
-  console.log("filter: ", filtered);
   clearData();
   showTasks();
 }
@@ -21,31 +53,32 @@ function showTasks() {
   taskData.forEach(function (item) {
     var div1 = document.createElement("div");
     div1.className = "task";
-    div1.id = uniqueId();
+    div1.id = item.id;
 
     var div2 = document.createElement("div");
     div2.className = "task-data";
 
-    var h6 = document.createElement("h6");
-    h6.textContent = item.content;
+    var h5 = document.createElement("h5");
+    h5.textContent = item.content;
 
     var p = document.createElement("p");
     p.textContent = item.created_at;
 
-    var editButton = document.createElement("button");
-    editButton.id = "edit-button";
-    editButton.textContent = "edit";
-
-    var deleteButton = document.createElement("button");
-    deleteButton.id = "delete-button";
-    deleteButton.textContent = "delete";
-    // Add event listener for delete-button here
+    var editButton = document.createElement("i");
+    editButton.className = "fa fa-pencil";
+    editButton.addEventListener("click", function handleClick(event) {
+        const taskId = event.target.parentElement.id;
+        editTask(taskId);
+      });
+    var deleteButton = document.createElement("i");
+    deleteButton.className = "fa fa-trash";
     deleteButton.addEventListener("click", function handleClick(event) {
-      deleteTask(event.target.parentElement.id);
+      const taskId = event.target.parentElement.id;
+      deleteTask(taskId);
     });
     itemList.appendChild(div1);
     div1.appendChild(div2);
-    div2.appendChild(h6);
+    div2.appendChild(h5);
     div2.appendChild(p);
     div1.appendChild(editButton);
     div1.appendChild(deleteButton);
@@ -60,7 +93,7 @@ function clearData() {
 }
 
 function addTask() {
-  const val = document.querySelector("input").value;
+  const val = document.querySelector("#create-task").value;
   const id = uniqueId();
   const timestamp = new Date().getTime();
   taskData.push({
